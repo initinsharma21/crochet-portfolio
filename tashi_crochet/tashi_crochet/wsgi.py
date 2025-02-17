9b6a2f7aa0ccc72d56afa1471974f56b0d417c60
@ankur.hanuai <nitin.hanuai@gmail.com>
2025-02-17 22:06:51 +0530

    "first"

--- tashi_crochet/tashi_crochet/wsgi.py
+++ tashi_crochet/tashi_crochet/wsgi.py
@@ -1,19 +1,10 @@
-"""
-WSGI config for tashi_crochet project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 import django
-django.setup()
-
-
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tashi_crochet.settings')
+
+django.setup()
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
